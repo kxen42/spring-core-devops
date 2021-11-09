a97c2e86eecc424ef927ca6236be14a9f0b4100c
Karen Christenson <kchristenson@illuminateed.net>
2021-11-09 13:42:02 -0600

    Ch. 11 Environment Variables

--- README.md
+++ README.md
@@ -9,10 +9,17 @@ An embedded Active MQ container is fired up for the JMS messages.
 
 ## Section 2. Externalizing Properties
 
-### Using Property Source
+### Using @PropertySource
 
 Use `@PropertySource` to read a properties file from the classpath during configuration.
 
 Configuring a `org.springframework.context.support.PropertySourcesPlaceholderConfigurer`. This has been around since
 Spring 3.1. It parses the Spring EL expressions in the `@Value` annotations to get you the value for the property.
 (Spring Boot probably does this for you.)
+
+### Using Environment Variables
+
+Uses `@Autowired Environment env;` pull in the .properties files without the placeholder configurer.
+Properties are fetched with `env.getProperty("property-name")` for optional properties.
+Required property with simple type conversion from String `env.getRequiredProperty("property-name", Integer.class)`.
+Properties can be set from environment variables; can set in IntelliJ Run Configuration.
---
+++ src/test/java/guru/test/config/external/props/ExternalPropsEnvironmentConfiguration.java
@@ -0,0 +1,30 @@
+package guru.test.config.external.props;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+import guru.springframework.test.jms.FakeJmsBroker;
+
+/**
+ * Demo getting properties from environment variables
+ */
+@Configuration
+@PropertySource("classpath:/testing.properties")
+public class ExternalPropsEnvironmentConfiguration {
+
+    @Autowired
+    Environment env;
+
+    @Bean
+    public FakeJmsBroker fakeJmsBrokerEnv() {
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+        fakeJmsBroker.setUrl(env.getProperty("guru.jms.server"));
+        fakeJmsBroker.setPort(env.getRequiredProperty("guru.jms.port", Integer.class));
+        fakeJmsBroker.setUser(env.getProperty("guru.jms.user"));
+        fakeJmsBroker.setPassword(env.getProperty("guru.jms.password"));
+        return fakeJmsBroker;
+    }
+}
