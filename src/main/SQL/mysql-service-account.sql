359666502451746e33b96812988289a3c0cf04cf
kxen42 <kxen42@users.noreply.github.com>
2023-01-05 17:06:01 -0600

    Replace user and fix driver warning

--- src/main/SQL/mysql-service-account.sql
+++ src/main/SQL/mysql-service-account.sql
@@ -3,7 +3,16 @@
  This is like the 'jvm' account that FBL used for the FastBridge application.
  */
 
+ /*
+  user only exists on localhost
+  */
+
 CREATE USER 'springframework'@'localhost' IDENTIFIED BY 'guru';
+
+/*
+ user only have permissions for springguru database
+
+ */
 
 GRANT SELECT ON springguru.* to 'springframework'@'localhost';
 GRANT INSERT ON springguru.* to 'springframework'@'localhost';
