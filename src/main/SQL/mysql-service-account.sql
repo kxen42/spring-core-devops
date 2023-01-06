960c07b8d5e64241f2ed7c770e49084d8301e647
kxen42 <kxen42@users.noreply.github.com>
2023-01-05 22:01:31 -0600

    add user creating details

--- src/main/SQL/mysql-service-account.sql
+++ src/main/SQL/mysql-service-account.sql
@@ -18,3 +18,19 @@ GRANT SELECT ON springguru.* to 'springframework'@'localhost';
 GRANT INSERT ON springguru.* to 'springframework'@'localhost';
 GRANT DELETE ON springguru.* to 'springframework'@'localhost';
 GRANT UPDATE ON springguru.* to 'springframework'@'localhost';
+
+/*
+Setup QA2 environment
+ */
+
+CREATE USER 'qa2'@'localhost' IDENTIFIED BY 'guru';
+
+/*
+ user only have permissions for springguru database
+
+ */
+
+GRANT SELECT ON springguru-qa2.* to 'qa2'@'localhost';
+GRANT INSERT ON springguru-qa2.* to 'qa2'@'localhost';
+GRANT DELETE ON springguru-qa2.* to 'qa2'@'localhost';
+GRANT UPDATE ON springguru-qa2.* to 'qa2'@'localhost';
